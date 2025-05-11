af5a8e66114b8704788c2580cd5168c504e879b2
Francisco Jaramillo <87986193+crusher922@users.noreply.github.com>
2025-05-11 14:34:57 -0500

    Update juanito.js

--- scripts/juanito.js
+++ scripts/juanito.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 7;
 const loaderFondo = new THREE.TextureLoader();
-loaderFondo.load('../images/fondo.jpg', function(texture) {
+loaderFondo.load('images/fondo.jpg', function(texture) {
     scene.background = texture;
 });
 

c2a6db72ba11ab10b9e40bfbcefc1cbe082de131
Francisco Jaramillo <87986193+crusher922@users.noreply.github.com>
2025-05-11 14:15:18 -0500

    Update marianita.js

--- scripts/marianita.js
+++ scripts/marianita.js
@@ -1,6 +1,8 @@
-import * as THREE from '/three';
+//import * as THREE from '/three';
 //import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from '/three/addons/controls/OrbitControls.js';
+//import { OrbitControls } from '/three/addons/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.150.1';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.150.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.150.1/examples/jsm/loaders/GLTFLoader.js'; 
 
 

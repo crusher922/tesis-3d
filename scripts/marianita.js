4726a09d5dec8c4aeec4bb74fabeb84315f7ab00
Francisco Jaramillo <87986193+crusher922@users.noreply.github.com>
2025-05-11 14:19:54 -0500

    Update marianita.js

--- scripts/marianita.js
+++ scripts/marianita.js
@@ -44,7 +44,7 @@ controls.dampingFactor = 0.05;
 
 // Cargar modelo
 const loader = new GLTFLoader();
-loader.load( '/models/MarianitaWeb.glb', function ( gltf ) {
+loader.load( 'models/MarianitaWeb.glb', function ( gltf ) {
     const model = gltf.scene;
     model.scale.set(1, 1, 1);
 

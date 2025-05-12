7c2e47084e40f2ef2327d30d29bed65a53f30586
Francisco Jaramillo <87986193+crusher922@users.noreply.github.com>
2025-05-11 22:30:52 -0500

    cambio color background modelo

--- scripts/marianita.js
+++ scripts/marianita.js
@@ -6,11 +6,11 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 4;
-//scene.background = new THREE.Color(0xf0f0f0); // Blanco
-const loaderFondo = new THREE.TextureLoader();
+scene.background = new THREE.Color(0x5b3512); // Blanco
+/* const loaderFondo = new THREE.TextureLoader();
 loaderFondo.load('../images/fondo.jpg', function(texture) {
     scene.background = texture;
-});
+}); */
 
 
 // Luz

b021b1b681b112086da13661c5ce4e94171b77a5
Francisco Jaramillo <87986193+crusher922@users.noreply.github.com>
2025-05-11 21:16:35 -0500

    cambio de rutas

--- scripts/marianita.js
+++ scripts/marianita.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from '../https://cdn.jsdelivr.net/npm/three@0.156.1/build/three.module.js';
+import { OrbitControls } from '../https://cdn.jsdelivr.net/npm/three@0.156.1/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../https://cdn.jsdelivr.net/npm/three@0.156.1/examples/jsm/loaders/GLTFLoader.js';
+
 
 // Preparar escena
 const scene = new THREE.Scene();
